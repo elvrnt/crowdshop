cf443669e56bd1dd24542b21809b934db3e40282
elvrnt <egorovlavrentii@gmail.com>
2026-06-15 13:04:50 +0300

    fix: remove collectstatic from Procfile, simplify static storage

--- crowdshop/settings.py
+++ crowdshop/settings.py
@@ -134,7 +134,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 _static_dir = BASE_DIR / 'static'
 STATICFILES_DIRS = [_static_dir] if os.path.isdir(_static_dir) else []
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
